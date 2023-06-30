681e2adf80d17315b89e551409cc87a550ece5db
Vert <alex.c.goble@gmail.com>
2023-06-30 13:33:25 -0400

    tweaks

--- src/api.py
+++ src/api.py
@@ -11,19 +11,10 @@ from src.logger import init_logging, init_sentry
 
 # from sentence_transformers import SentenceTransformer
 
-init_logging()
-init_sentry(config.SENTRY_DSN)
-
 log = logging.getLogger(__name__)
-log.info(f"Downloading/fetching model: {config.MODEL_NAME}")
+
 
 app = FastAPI(title="GPT API")
-model = GPT4All(
-    model_name=config.MODEL_NAME,
-    model_path=config.MODEL_PATH,
-    n_threads=int(config.THREADS) if config.THREADS else None,
-)
-# embedder = SentenceTransformer(config.EMBED_MODEL)
 
 
 class ChatInput(BaseModel):
@@ -76,5 +67,16 @@ async def chat(payload: ChatInput) -> dict:
 
 @app.on_event("startup")
 async def startup_event():
+    def _run():
+        global model
+        model = GPT4All(
+            model_name=config.MODEL_NAME,
+            model_path=config.MODEL_PATH,
+            n_threads=int(config.THREADS) if config.THREADS else None,
+        )
+        # embedder = SentenceTransformer(config.EMBED_MODEL)
+
     init_logging()
     init_sentry(config.SENTRY_DSN)
+    log.info(f"Downloading/fetching model: {config.MODEL_NAME}")
+    await asyncio.to_thread(_run)
