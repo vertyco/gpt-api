40e37a9e815fc24cfe7d91456b88882c19c80f45
Vert <alex.c.goble@gmail.com>
2023-06-30 13:30:48 -0400

    tweaks

--- src/api.py
+++ src/api.py
@@ -3,6 +3,7 @@ import logging
 from typing import List, Optional, Union
 
 from fastapi import FastAPI
+from gpt4all import GPT4All
 from pydantic import BaseModel
 
 import src.config as config
@@ -10,9 +11,19 @@ from src.logger import init_logging, init_sentry
 
 # from sentence_transformers import SentenceTransformer
 
+init_logging()
+init_sentry(config.SENTRY_DSN)
 
 log = logging.getLogger(__name__)
+log.info(f"Downloading/fetching model: {config.MODEL_NAME}")
+
 app = FastAPI(title="GPT API")
+model = GPT4All(
+    model_name=config.MODEL_NAME,
+    model_path=config.MODEL_PATH,
+    n_threads=int(config.THREADS) if config.THREADS else None,
+)
+# embedder = SentenceTransformer(config.EMBED_MODEL)
 
 
 class ChatInput(BaseModel):
@@ -67,16 +78,3 @@ async def chat(payload: ChatInput) -> dict:
 async def startup_event():
     init_logging()
     init_sentry(config.SENTRY_DSN)
-
-    global model
-    log.info(f"Downloading/fetching model: {config.MODEL_NAME}")
-    from gpt4all import GPT4All
-
-    model = GPT4All(
-        model_name=config.MODEL_NAME,
-        model_path=config.MODEL_PATH,
-        n_threads=int(config.THREADS) if config.THREADS else None,
-    )
-
-    # global embedder
-    # embedder = SentenceTransformer(config.EMBED_MODEL)
