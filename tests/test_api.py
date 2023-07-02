80ebbd85996d389254777f58e4c64a1b4c3d4f3b
Vert <alex.c.goble@gmail.com>
2023-07-01 23:29:11 -0400

    tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,13 +1,17 @@
+import asyncio
 import unittest
 
 from fastapi.testclient import TestClient
 
-from src.api import app
+from src.api import app, startup_event
 
 
 class TestAPI(unittest.TestCase):
     def setUp(self):
         self.client = TestClient(app)
+        # Manually run the startup event
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(startup_event())
 
     def test_chat(self):
         response = self.client.post(
