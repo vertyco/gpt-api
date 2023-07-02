47d6d128117daf17a6280489cc7d71fc53c802c9
Vert <alex.c.goble@gmail.com>
2023-07-02 18:18:58 -0400

    cleaner prompting

--- src/utils.py
+++ src/utils.py
@@ -1,33 +1,13 @@
 def compile_messages(messages: list[dict]) -> str:
-    system = ""
-    context = ""
     prompt = ""
-    for message in messages:
-        if message["role"] == "system":
-            system += message["content"] + "\n"
 
     for message in messages:
-        if message["role"] == "system":
-            continue
-        content = message["content"].strip()
-        if "### Context:" in content:
-            context += f"{content.replace('### Context:', '').strip()}\n"
-        elif message["role"] == "user":
-            prompt += f"### Prompt: {content}\n"
-        elif message["role"] == "assistant":
-            prompt += f"### Response: {content}\n"
+        role = message["role"]
+        content = message["content"]
+        prompt += f"### {role.upper()}: {content.strip()}\n"
 
-    final = ""
-    if system:
-        final += f"### Instruction: {system}\n"
-    if context:
-        final += f"### Context: {context}\n"
-    if prompt:
-        final += prompt
-
-    final += "\n### Response:"
-
-    return final.strip()
+    prompt += "### Response:"
+    return prompt
 
 
 def compile_qa_messages(messages: list[dict]) -> tuple[str, str]:
